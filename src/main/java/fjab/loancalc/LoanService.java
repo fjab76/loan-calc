510af4458cd07a2c13ec903f36c522a80a361094
Francisco Alvarez <fjab76@gmail.com>
2015-07-26 19:27:54 +0100

    removing code commented out

--- src/main/java/fjab/loancalc/LoanService.java
+++ src/main/java/fjab/loancalc/LoanService.java
@@ -4,10 +4,6 @@ import fjab.loancalc.french.model.RepaymentPlan;
 
 public interface LoanService {
 	
-	//public double workOutPeriodicPayment(double interestRateForEveryPeriod,double capitalToPayBack,int numberPaymentPeriods);
-	
 	public void calculateRepaymentPlan(RepaymentPlan repaymentPlan) throws Exception;
-
-	//void calculateRepaymentPlanWithOverpayment(RepaymentPlan repaymentPlan) throws Exception;
 
 }
