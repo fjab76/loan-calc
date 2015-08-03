5977fd2777a051852c9142e234f1c5516997839c
Francisco Alvarez <fjab76@gmail.com>
2015-08-04 00:43:22 +0100

    adding description and counter fields to illustrate text change events

--- src/main/java/fjab/loancalc/view/LoanForm.java
+++ src/main/java/fjab/loancalc/view/LoanForm.java
@@ -6,8 +6,11 @@ import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.util.BeanItem;
+import com.vaadin.event.FieldEvents.TextChangeEvent;
+import com.vaadin.ui.AbstractTextField.TextChangeEventMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 
@@ -18,16 +21,29 @@ import com.vaadin.ui.Button.ClickEvent;
 
 public class LoanForm extends FormLayout {
 	
+	private TextField description = new TextField("Write a description for this loan:");
+	// Counter for input length
+	private Label counter = new Label();
+	
 	private TextField annualInterestRate = new TextField("Annual interest rate:");
 	private TextField loanAmount = new TextField("Loan amount:");
 	private TextField numberAnnualPayments = new TextField("Number annual payments:");
 	private TextField loanLengthYears = new TextField("Loan length in years:");
 	private TextField loanLengthMonths = new TextField("Loan length in months:");
 	private Button okButton = new Button("OK");
-	
 	FieldGroup fieldGroup;
 
 	public LoanForm(){
+		
+		//Customising form layout
+		setSizeUndefined();
+		setMargin(true);
+		
+		//Customising form elements
+		description.setMaxLength(20);
+		description.addTextChangeListener(event -> createTextChangeListener(event));
+		description.setTextChangeEventMode(TextChangeEventMode.EAGER);
+		counter.setValue(description.getValue().length() +" of " + description.getMaxLength());
 		
 		annualInterestRate.setNullRepresentation("");
 		loanAmount.setNullRepresentation("");
@@ -35,25 +51,28 @@ public class LoanForm extends FormLayout {
 		loanLengthYears.setNullRepresentation("");
 		loanLengthMonths.setNullRepresentation("");
 		
-		setSizeUndefined();
-		setMargin(true);
+		okButton.addClickListener(event -> createClickListener(event));
 		
+		//Binding form elements to bean
 		fieldGroup = new BeanFieldGroup<>(LoanBean.class);
 		fieldGroup.setItemDataSource(new BeanItem<>(new LoanBean()));
 		fieldGroup.bindMemberFields(this);
 		
+		//Adding elements to the form
+		addComponent(description);
+		addComponent(counter);
 		addComponent(annualInterestRate);
 	    addComponent(loanAmount);
 	    addComponent(numberAnnualPayments);
 	    addComponent(loanLengthYears);
 	    addComponent(loanLengthMonths);
 	    addComponent(okButton);
-	    okButton.addClickListener(event -> createListenerLogic(event));
+	    
 	    
 		
 	}
 	
-	private void createListenerLogic(ClickEvent event) {
+	private void createClickListener(ClickEvent event) {
 		try 
 		{
           fieldGroup.commit();
@@ -73,5 +92,10 @@ public class LoanForm extends FormLayout {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private void createTextChangeListener(TextChangeEvent event){
+		int len = event.getText().length();
+        counter.setValue(len + " of " + description.getMaxLength());
 	}
 }
